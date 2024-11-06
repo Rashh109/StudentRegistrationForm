9b9681c67c856c4a786246ead090769568cc0256
Rashh109 <rashasodiya@gmail.com>
2024-11-06 18:59:46 +0530

    update files

---
+++ LabSheet1/JSfile1.js
@@ -0,0 +1,10 @@
+function validateForm(event){
+    var form=event.target;
+    if(!form.checkValidity()){
+        event.preventDefault();
+    } else if (form.name.value===""){
+        alert("Enter your name");
+    } else if(form.email.value==="" || form.email.validity.valid){
+        alert("Enter valid email");
+    }
+}
